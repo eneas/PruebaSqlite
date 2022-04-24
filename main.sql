2592675224d7a11f4327af9ed904e3a1fd705dba
Eneas Labra Villar <eneaslabra@gmail.com>
2022-04-24 18:54:31 +0000

    Version inicial

--- main.sql
+++ main.sql
@@ -1,1 +1,16 @@
-SELECT "Hello world!";
+CREATE TABLE Asignaturas(
+  AsignaturaID int,
+  Nombre varchar(255)
+)
+  ;
+CREATE TABLE Examenes (
+  ExamenID int,
+  Titulo varchar(255),
+  Fecha date,
+  Hora time,
+  AsignaturaID int
+);
+INSERT INTO Asignaturas(AsignaturaID,Nombre) 
+  VALUES(1,"Operaciones Básicas en la Industria Química");
+
+SELECT * from Asignaturas;
